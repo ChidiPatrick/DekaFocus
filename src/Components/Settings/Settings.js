64b24a6e5d42a2dfbb5299d2ca8f1536b470683c
Patrick Chidiebere <okaforpatrick302@gmail.com>
2022-10-23 12:13:42 +0100

    Implemented pomodoro time selection

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,129 +1,226 @@
-import React, { useState } from "react"
-import styles from "./Settings.module.scss"
-import {Link} from "react-router-dom"
-import NavButton from "../NavButtons/NavButton"
-import { FaChevronLeft,FaChevronRight } from "react-icons/fa";
-import {showMinutes,hideMinutes} from "../Settings/SettingsSlice"
-import {useDispatch,useSelector} from "react-redux"
-const Settings =  (props) => {
-    const dispatch = useDispatch();
-    const selected = useSelector((state) => state.settings.selected)
-    const pomodoroMinute = useSelector((state) => state.settings.pomodoroLength)
-    // const [selected, setSelected] = useState(false)
-    console.log(pomodoroMinute);
-    const Minutes = [];
-    console.log(Minutes);
-    for(let i = 0; i <= 60; i++){
-        Minutes.push( i + 1)
-    }
-    const toggleSelectEl = () => {
-        if(selected) {
-            dispatch(hideMinutes())
-        }
-        if(!selected){
-            dispatch(showMinutes())
-        }
-    }
-    return (<div className={styles.Setting}>
-        <div className={styles.HeaderWrapper}>
-            <Link to={-1} className={styles.navLink}><FaChevronLeft className={styles.iconBack}/></Link>
-            <h3 className={styles.SettingHeader}>Settings</h3>
-        </div>
-        <div className={styles.accountDetails}>
-            <figure className={styles.Avatar}></figure>
-            <div className={styles.userName}>Mr. Somebody</div>
-            <FaChevronRight className={styles.iconBack}/>
-        </div>
-        <div className={styles.projects}>projects</div>
-        <div className={styles.AlarmSettings}>
-            <div className={styles.linkWrapper}>
-                <Link to="workAlarm" className={styles.workAlarm}>Work Alarm</Link>
-                <div className={styles.alarmTone}>
-                    <span className={styles.alarm}>Bell2</span> 
-                    <FaChevronRight className={styles.iconBack}/>
-                </div>
-            </div>
-            <div className={styles.linkWrapper}>
-                <Link to="workAlarm" className={styles.workAlarm}>Break Alarm</Link>
-                <div className={styles.alarmTone}>
-                    <span className={styles.alarm}>Bell3</span> 
-                    <FaChevronRight className={styles.iconBack}/>
-                </div>
-            </div>
-            <div className={styles.linkWrapper}>
-                <Link to="workAlarm" className={styles.workAlarm}>White Noise</Link>
-                <div className={styles.alarmTone}>
-                    <span className={styles.alarm}>Bell3</span> 
-                    <FaChevronRight className={styles.iconBack}/>
-                </div>
-            </div>
-            
-        </div>
-        <div className={styles.timeSettingsWrapper}>
-            <div className={styles.pomodoroLength}>
-                    <span className={styles.typeOfBreak}>Pomodoro Length</span>
-                    <div className={styles.pomodoroDuration}>
-                        <span>45 Minutes</span> 
-                        <FaChevronLeft className={styles.iconBack}/> 
-                    </div>
-            </div>
-            <div className={styles.pomodoroLength}>
-                    <span className={styles.typeOfBreak}>Short Break Length</span>
-                    <div className={styles.pomodoroDuration}>
-                        <span>45 Minutes</span> 
-                        <FaChevronLeft className={styles.iconBack}/> 
-                    </div>
-            </div>
-            <div className={styles.pomodoroLength}>
-                    <span className={styles.typeOfBreak}>Long Break Length</span>
-                    <div className={styles.pomodoroDuration}>
-                        <span>45 Minutes</span> 
-                        <FaChevronLeft className={styles.iconBack}/> 
-                    </div>
-            </div>
-            
-            <div className={styles.pomodoroLength}>
-                <span className={styles.typeOfBreak}>Long Break After</span>
-                <div className={styles.pomodoroDuration}>
-                    <span>45 Minutes</span> 
-                    <FaChevronLeft className={styles.iconBack}/> 
-                </div>
-            </div>
-            <form className={styles.minuteList}>
-                <label htmlFor ="minutes" className={styles.minutesLabel} onClick = {toggleSelectEl}>
-                    <span>Pomodoro Length</span>
-                    <span>45 Minutes</span>
-                </label>
-                {/* <input list = "minutes" id ="minutes" className={styles.minutes}/> */}
-                    <select className = {selected ? styles.selectMinutes : styles.hidden} >
-                        {
-                            Minutes.map((minute,i) => {
-                                return <option className={styles.minuteOption} value= {minute} key= {i}>{minute}</option>
-                            })
-                        }
-                    </select>x
-            </form>
-            {/* <FormControl fullWidth variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="demo-simple-select-label" className={styles.inputLabel}>Pomodoro Length</InputLabel>
-                <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
-                <Select
-                        labelId="demo-simple-select-standard-label"
-                        id="demo-simple-select-standard"
-                        // value={age}
-                        // onChange={handleChange}
-                        // label="Age"
-                >
-                    {
-                        Minutes.map((minute,i ) => {
-                            return <MenuItem value={minute} key = {i}>{minute}</MenuItem>
-                        })
-                    }
-                    
-                    
-               </Select>
-            </FormControl> */}
-            
-        </div>
-    </div>)
-}
-export default Settings
+import React, { useState, useRef } from 'react';
+import styles from './Settings.module.scss';
+import { Link } from 'react-router-dom';
+import NavButton from '../NavButtons/NavButton';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import {
+	showMinutes,
+	hideMinutes,
+	updateTime,
+	showLongBreak,
+	hideLongBreak,
+	showLongBreakAfter,
+	hideLongBreakAfter,
+	showShortBreak,
+	hideShortBreak,
+	updateLongBreakAfterTime,
+	updateLongBreakTime,
+	updatePomodoroTime,
+	updateShortBreakTime
+} from '../Settings/SettingsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+const Settings = (props) => {
+	///////////////////
+	///GLOBALS/////////
+	const dispatch = useDispatch();
+	////Select Display states/////
+	const selected = useSelector((state) => state.settings.selected);
+	const pomodoroLengthSelected = useSelector((state) => state.settings.pomodoroLengthSelected);
+	const shortBreakLengthSelected = useSelector((state) => state.settings.shortBreakLengthSelected);
+	const longBreakLengthSelected = useSelector((state) => state.settings.longBreakLengthSelected);
+	const longBreakAfterLengthSelected = useSelector((state) => state.settings.longBreakAfterSelected);
+	/// Get Pomodoro Durations //////
+	const pomodoroCurrLength = useSelector((state) => state.settings.pomodoroCurrLength);
+	const shortBreakCurrLength = useSelector((state) => state.settings.shortBreakCurrLength);
+	const longBreakCurrLength = useSelector((state) => state.settings.longBreakCurrLength);
+	const longBreakAfterCurrLength = useSelector((state) => state.settings.longBreakAfterCurrLength);
+	///////////////////////////////
+	const selectRef = useRef();
+	const Minutes = [];
+	let selectedTime = 45;
+	for (let i = 0; i <= 60; i++) {
+		Minutes.push(i + 1);
+	}
+	console.log(pomodoroLengthSelected);
+	const togglePomodoroSelect = () => {
+		if (!pomodoroLengthSelected) {
+			dispatch(showMinutes());
+		}
+		if (pomodoroLengthSelected) {
+			dispatch(hideMinutes());
+		}
+	};
+	const toggleShortBreakSelect = () => {
+		if (!shortBreakLengthSelected) {
+			dispatch(showShortBreak());
+		}
+		if (shortBreakLengthSelected) {
+			dispatch(hideShortBreak());
+		}
+	};
+
+	const toggleLongBreakAfter = () => {
+		if (!longBreakAfterLengthSelected) {
+			dispatch(showLongBreakAfter());
+			console.log(longBreakAfterLengthSelected);
+		}
+		if (longBreakAfterLengthSelected) {
+			dispatch(hideLongBreakAfter());
+		}
+	};
+	const toggleLongBreak = () => {
+		if (!longBreakLengthSelected) {
+			dispatch(showLongBreak());
+			console.log(longBreakLengthSelected);
+		}
+		if (longBreakLengthSelected) {
+			dispatch(hideLongBreak());
+		}
+	};
+
+	///////////////////////////////////
+	//GET SELECTED MINUTE
+	const getPomodoroTime = (e) => {
+		let curValue = e.target.value;
+		console.log(typeof selectedTime);
+		dispatch(hideMinutes());
+		dispatch(updatePomodoroTime({ currTime: curValue }));
+	};
+	const getShortBreakTime = (e) => {
+		let curValue = e.target.value;
+		dispatch(hideShortBreak());
+		dispatch(updateShortBreakTime({ currTime: curValue }));
+	};
+	const getLongBreakTime = (e) => {
+		let curValue = e.target.value;
+		dispatch(hideLongBreak());
+		dispatch(updateLongBreakTime({ currTime: curValue }));
+	};
+	const getLongBreakAfterTime = (e) => {
+		let curValue = e.target.value;
+		dispatch(hideLongBreakAfter());
+		dispatch(updateLongBreakAfterTime({ currTime: curValue }));
+	};
+	return (
+		<div className={styles.Setting}>
+			<div className={styles.HeaderWrapper}>
+				<Link to={-1} className={styles.navLink}>
+					<FaChevronLeft className={styles.iconBack} />
+				</Link>
+				<h3 className={styles.SettingHeader}>Settings</h3>
+			</div>
+			<div className={styles.accountDetails}>
+				<figure className={styles.Avatar} />
+				<div className={styles.userName}>Mr. Somebody</div>
+				<FaChevronRight className={styles.iconBack} />
+			</div>
+			<div className={styles.projects}>projects</div>
+			<div className={styles.AlarmSettings}>
+				<div className={styles.linkWrapper}>
+					<Link to="workAlarm" className={styles.workAlarm}>
+						Work Alarm
+					</Link>
+					<div className={styles.alarmTone}>
+						<span className={styles.alarm}>Bell2</span>
+						<FaChevronRight className={styles.iconBack} />
+					</div>
+				</div>
+				<div className={styles.linkWrapper}>
+					<Link to="workAlarm" className={styles.workAlarm}>
+						Break Alarm
+					</Link>
+					<div className={styles.alarmTone}>
+						<span className={styles.alarm}>Bell3</span>
+						<FaChevronRight className={styles.iconBack} />
+					</div>
+				</div>
+				<div className={styles.linkWrapper}>
+					<Link to="workAlarm" className={styles.workAlarm}>
+						White Noise
+					</Link>
+					<div className={styles.alarmTone}>
+						<span className={styles.alarm}>Bell3</span>
+						<FaChevronRight className={styles.iconBack} />
+					</div>
+				</div>
+			</div>
+			<div className={styles.timeSettingsWrapper}>
+				{/* <div className={styles.pomodoroLength}>
+					<span className={styles.typeOfBreak}>Long Break After</span>
+					<div className={styles.pomodoroDuration}>
+						<span>45 Minutes</span>
+						<FaChevronLeft className={styles.iconBack} />
+					</div>
+				</div>  */}
+				<form className={styles.minuteList}>
+					<label htmlFor="minutes" className={styles.minutesLabel} onClick={togglePomodoroSelect}>
+						<span>Pomodoro Length</span>
+						<span>{pomodoroCurrLength} Minutes</span>
+					</label>
+					<select
+						className={pomodoroLengthSelected ? styles.selectMinutes : styles.hidden}
+						onChange={getPomodoroTime}
+					>
+						{Minutes.map((minute, i) => {
+							return (
+								<option ref={selectRef} className={styles.minuteOption} value={minute} key={i}>
+									{minute}
+								</option>
+							);
+						})}
+					</select>
+
+					<label htmlFor="minutes" className={styles.minutesLabel} onClick={toggleShortBreakSelect}>
+						<span>Short Break Length</span>
+						<span>{shortBreakCurrLength} Minutes</span>
+					</label>
+					<select
+						className={shortBreakLengthSelected ? styles.selectMinutes : styles.hidden}
+						onChange={getShortBreakTime}
+					>
+						{Minutes.map((minute, i) => {
+							return (
+								<option ref={selectRef} className={styles.minuteOption} value={minute} key={i}>
+									{minute}
+								</option>
+							);
+						})}
+					</select>
+					<label htmlFor="minutes" className={styles.minutesLabel} onClick={toggleLongBreak}>
+						<span>Long Break Length</span>
+						<span>{longBreakCurrLength} Minutes</span>
+					</label>
+					<select
+						className={longBreakLengthSelected ? styles.selectMinutes : styles.hidden}
+						onChange={getLongBreakTime}
+					>
+						{Minutes.map((minute, i) => {
+							return (
+								<option ref={selectRef} className={styles.minuteOption} value={minute} key={i}>
+									{minute}
+								</option>
+							);
+						})}
+					</select>
+					<label htmlFor="minutes" className={styles.minutesLabel} onClick={toggleLongBreakAfter}>
+						<span>Long Break After</span>
+						<span>{longBreakAfterCurrLength} Pomodoros</span>
+					</label>
+					<select
+						className={longBreakAfterLengthSelected ? styles.selectMinutes : styles.hidden}
+						onChange={getLongBreakAfterTime}
+					>
+						{Minutes.map((minute, i) => {
+							return (
+								<option ref={selectRef} className={styles.minuteOption} value={minute} key={i}>
+									{minute}
+								</option>
+							);
+						})}
+					</select>
+				</form>
+			</div>
+		</div>
+	);
+};
+export default Settings;
--- src/Components/Settings/SettingsSlice.js
+++ src/Components/Settings/SettingsSlice.js
@@ -1,20 +1,74 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
-    selected: false,
-    pomodoroLength: 45,
-}
+	pomodoroLengthSelected: false,
+	shortBreakLengthSelected: false,
+	longBreakLengthSelected: false,
+	longBreakAfterSelected: false,
+	pomodoroCurrLength: 45,
+	shortBreakCurrLength: 10,
+	longBreakCurrLength: 15,
+	longBreakAfterCurrLength: 4
+};
 
 const SettingSlice = createSlice({
-    name: "settings",
-    initialState,
-    reducers: {
-        showMinutes(state, action){
-            state.selected = true
-        },
-        hideMinutes(state, action){
-            state.selected = false
-        }
-    }
-})
-export const {showMinutes,hideMinutes} = SettingSlice.actions;
-export default SettingSlice.reducer
+	name: 'settings',
+	initialState,
+	reducers: {
+		showMinutes(state, action) {
+			state.pomodoroLengthSelected = true;
+		},
+		hideMinutes(state, action) {
+			state.pomodoroLengthSelected = false;
+		},
+		showShortBreak(state, action) {
+			state.shortBreakLengthSelected = true;
+		},
+		hideShortBreak(state, action) {
+			state.shortBreakLengthSelected = false;
+		},
+		showLongBreakAfter(state, action) {
+			state.longBreakAfterSelected = true;
+		},
+		hideLongBreakAfter(state, action) {
+			state.longBreakAfterSelected = false;
+		},
+		showLongBreak(state, action) {
+			state.longBreakLengthSelected = true;
+		},
+		hideLongBreak(state, action) {
+			state.longBreakLengthSelected = false;
+		},
+		updatePomodoroTime(state, action) {
+			const { currTime } = action.payload;
+			state.pomodoroCurrLength = currTime;
+		},
+		updateShortBreakTime(state, action) {
+			const { currTime } = action.payload;
+			state.shortBreakCurrLength = currTime;
+		},
+		updateLongBreakTime(state, action) {
+			const { currTime } = action.payload;
+			state.longBreakCurrLength = currTime;
+		},
+		updateLongBreakAfterTime(state, action) {
+			const { currTime } = action.payload;
+			state.longBreakAfterCurrLength = currTime;
+		}
+	}
+});
+export const {
+	showMinutes,
+	hideMinutes,
+	updateTime,
+	showLongBreak,
+	hideLongBreak,
+	showLongBreakAfter,
+	hideLongBreakAfter,
+	showShortBreak,
+	hideShortBreak,
+	updateLongBreakAfterTime,
+	updateLongBreakTime,
+	updatePomodoroTime,
+	updateShortBreakTime
+} = SettingSlice.actions;
+export default SettingSlice.reducer;
