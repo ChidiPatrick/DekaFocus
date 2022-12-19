ad1a0e9b81a8f834a25262da9e64f59704a21bda
Patrick Chidiebere <okaforpatrick302@gmail.com>
2022-12-19 15:56:20 +0100

    Fixing bug

--- src/Components/FrontPage/FrontPage.js
+++ src/Components/FrontPage/FrontPage.js
@@ -55,7 +55,7 @@ const FrontPage = ({ expiryTimestamp }) => {
   const pomodoroTime = useSelector(state => state.settings.pomodoroCurrLength)
   const countDownRunning = useSelector(state => state.frontPage.countDownRunning)
 
-   console.log(typeof tone);
+   console.log( tone);
   const workAlarm = new UIFx(Toness[tone], { volume: 0.4, throttleMs: 100 });
   console.log(tone);
   const navigate = useNavigate()
